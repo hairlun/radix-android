74b3644b23f98f190a7502142a815ace60bf00e5
hairlun <hairlun@qq.com>
2016-06-20 18:01:42 +0800

    显示gif图片

--- src/com/patr/radix/fragment/UnlockFragment.java
+++ src/com/patr/radix/fragment/UnlockFragment.java
@@ -1,6 +1,8 @@
 package com.patr.radix.fragment;
 
 import com.patr.radix.R;
+import com.patr.radix.view.GifView;
+import com.patr.radix.view.TitleBarView;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -13,6 +15,10 @@ import android.view.ViewGroup.LayoutParams;
 import android.widget.TextView;
 
 public class UnlockFragment extends Fragment {
+    
+    private TitleBarView titleBarView;
+    
+    private GifView gifView;
 
 	@Override
 	public void onAttach(Activity activity) {
@@ -24,6 +30,10 @@ public class UnlockFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_unlock, container, false);
+		titleBarView = (TitleBarView) view.findViewById(R.id.unlock_titlebar);
+		titleBarView.hideBackBtn();
+		gifView = (GifView) view.findViewById(R.id.unlock_gv);
+		gifView.setMovieResource(R.raw.shake_shake);
 		return view;
 	}
 
