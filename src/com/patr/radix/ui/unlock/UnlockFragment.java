a963184b3d71ff0be97a59277993f4b221b29fa5
hairlun <hairlun@qq.com>
2016-10-12 19:47:20 +0800

    完善

--- src/com/patr/radix/ui/unlock/UnlockFragment.java
+++ src/com/patr/radix/ui/unlock/UnlockFragment.java
@@ -1228,6 +1228,7 @@ public class UnlockFragment extends Fragment implements OnClickListener,
 
                 if (list.size() <= 0) {
                     ToastUtil.showShort(context, "没找到与之匹配的门禁设备！");
+                    loadingDialog.dismiss();
                 }
                 mScanning = false;
             }
@@ -1263,6 +1264,7 @@ public class UnlockFragment extends Fragment implements OnClickListener,
 
                 if (list.size() <= 0) {
                     ToastUtil.showShort(context, "没找到与之匹配的门禁设备！");
+                    loadingDialog.dismiss();
                 }
                 mScanning = false;
             }
