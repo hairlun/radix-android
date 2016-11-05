a32b8dc9a4125b5705a910da214cf7bcc9cddca3
hairlun <hairlun@qq.com>
2016-11-05 10:29:45 +0800

    修改云通讯(1)

--- src/com/yuntongxun/ecdemo/ui/chatting/base/OnListViewTopListener.java
+++
@@ -1,5 +0,0 @@
-package com.yuntongxun.ecdemo.ui.chatting.base;
-public abstract interface OnListViewTopListener
-{
-  public abstract boolean getIsListViewToTop();
-}
--- src/com/yuntongxun/ecdemo/ui/chatting/base/OnRefreshAdapterDataListener.java
+++
@@ -1,5 +0,0 @@
-package com.yuntongxun.ecdemo.ui.chatting.base;
-public abstract interface OnRefreshAdapterDataListener
-{
-  public abstract void refreshData();
-}
